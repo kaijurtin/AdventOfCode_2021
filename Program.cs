f18dead6a48bc68c79e83395805e047ff49dc53a
Kai Jurtin <kai@jurtin.de>
2021-12-01 06:57:29 +0100

    created and solved day1 part 1

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AdventOfCodeStartProject
+{
+    static class AdventOfCode
+    {
+        //this field contains whats written in your input.txt
+        //each line in the list of strings represents one line in the txt file
+        //copy the data you get on the adventofcode website into the txt file and start coding!
+        private static List<string> _inputData;
+
+        static void Main(string[] args)
+        {
+            //EXAMPLE CODE
+            //hit F5 to run it
+
+            //read data from input.txt and write it into inputdata field
+            readInput();
+            //var intlist = _inputData.Select(i => int.Parse(i)).ToList();
+            //lets write the contents of the input.txt into the console
+
+            //Console.WriteLine("Content of input.txt:");
+            //_inputData.ForEach(i => Console.WriteLine(i));
+            //Console.WriteLine($"--END OF FILE--{Environment.NewLine}");
+            List<int> _inputDataINT = _inputData.ConvertAll(item => int.Parse(item));
+
+            int countIncrease = 0;
+
+            for (int i = 0; i < _inputDataINT.Count-1; i++)
+            {
+                if (_inputDataINT[i+1] > _inputDataINT[i])
+                countIncrease++;
+
+
+                //Console.ReadKey();
+
+            }                
+                Console.WriteLine("Increased Depth: " + countIncrease);
+
+                //keep console open
+                Console.WriteLine("Hit any key to close this window...");
+                Console.ReadKey();
+            
+        }
+        private static void readInput()
+        {
+            _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
+        }
+    }
+}
