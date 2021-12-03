357132342335191d3462d51afa70823faa20dec8
kaijurtin <92209432+kaijurtin@users.noreply.github.com>
2021-12-03 13:19:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,62 +20,82 @@ namespace AdventOfCodeStartProject
 
             //read data from input.txt and write it into inputdata field
             readInput();
-            
-            
+
+
             int countzero = 0;
             int countone = 0;
-            string gammarate= string.Empty;
-            string epsilonrate = string.Empty;
+            string oxrat = string.Empty;
+            string corat = string.Empty;
             int powerconsumption = 0;
-                
-                for(int j = 0; j < _inputData.First().Length; j++)
-                {
-                
+            List<string> oxygen = _inputData.ToList();
+            List<string> cotwo = _inputData.ToList();
+
+            for (int j = 0; j < _inputData.First().Length; j++)
+            {
+
                 countzero = 0;
                 countone = 0;
 
                 for (int i = 0; i < _inputData.Count; i++)
-                    { 
+                {
                     string s = _inputData[i];
                     char c = s[j];
-                    if (c=='0')
+                    if (c == '0')
                         countzero++;
                     else countone++;
-                    }
+                }
 
-                if (countone > countzero)
-                {
-                    gammarate += "1";
-                    epsilonrate += "0";
-                }
-                else
-                {
-                    gammarate += "0";
-                    epsilonrate += "1";
-                }
-                }
+                //for (int i = 0; i < _inputData.Count; i++)
+                //{
+                if (countone >= countzero && oxygen.Count > 0)
+                        oxygen = oxygen.Where(x => x[j] == '1').ToList();
+                if (countone < countzero && oxygen.Count > 0)
+                        oxygen = oxygen.Where(x => x[j] == '0').ToList();
+                if (countone >= countzero && cotwo.Count > 0)
+                    cotwo = cotwo.Where(x => x[j] == '0').ToList();
+                if (countone < countzero && cotwo.Count > 0)
+                    cotwo = cotwo.Where(x => x[j] == '1').ToList();
+
+                 oxrat = oxygen.Select(x => x.Count = 1).Select(x => x);                        
+                    if(cotwo.Count == 1)
+                        cotwo.First().ToString();   
+            
+
+
             int decimalgamma = Convert.ToInt32(gammarate, 2);
             int decimalepsilon = Convert.ToInt32(epsilonrate, 2);
-            powerconsumption = decimalepsilon * decimalgamma;
+            //powerconsumption = decimalepsilon * decimalgamma;
 
 
 
-            Console.WriteLine(gammarate + "/n" + decimalgamma);
-            Console.WriteLine(epsilonrate + "/n" + decimalepsilon);    
-            Console.WriteLine(powerconsumption);
+
+            //Console.WriteLine(gammarate + "/n" + decimalgamma);
+            //Console.WriteLine(epsilonrate + "/n" + decimalepsilon);    
+            //Console.WriteLine(powerconsumption);
 
 
 
+        
+
+            foreach (string i in oxygen)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (string i in cotwo)
+            {
+                Console.WriteLine(i);
+            }
             Console.WriteLine("Hit any key to close this window...");
             Console.ReadKey();
+
         }
+
 
 
 
         private static void readInput()
-        {
-            _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
+            {
+                _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
+            }
         }
     }
-}
-
