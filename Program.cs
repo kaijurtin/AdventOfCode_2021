06c2f553f4b06ee7fede7303e1eaea90b8c6daec
kaijurtin <92209432+kaijurtin@users.noreply.github.com>
2021-12-03 10:49:46 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,6 +12,7 @@ namespace AdventOfCodeStartProject
         //copy the data you get on the adventofcode website into the txt file and start coding!
         private static List<string> _inputData;
 
+
         static void Main(string[] args)
         {
             //EXAMPLE CODE
@@ -19,35 +20,62 @@ namespace AdventOfCodeStartProject
 
             //read data from input.txt and write it into inputdata field
             readInput();
-            //var intlist = _inputData.Select(i => int.Parse(i)).ToList();
-            //lets write the contents of the input.txt into the console
-
-            //Console.WriteLine("Content of input.txt:");
-            //_inputData.ForEach(i => Console.WriteLine(i));
-            //Console.WriteLine($"--END OF FILE--{Environment.NewLine}");
-            List<int> _inputDataINT = _inputData.ConvertAll(item => int.Parse(item));
-
-            int countIncrease = 0;
-
-            for (int i = 0; i < _inputDataINT.Count-1; i++)
-            {
-                if (_inputDataINT[i+1] > _inputDataINT[i])
-                countIncrease++;
-
-
-                //Console.ReadKey();
-
-            }                
-                Console.WriteLine("Increased Depth: " + countIncrease);
-
-                //keep console open
-                Console.WriteLine("Hit any key to close this window...");
-                Console.ReadKey();
             
+            
+            int countzero = 0;
+            int countone = 0;
+            string gammarate= string.Empty;
+            string epsilonrate = string.Empty;
+            int powerconsumption = 0;
+                
+                for(int j = 0; j < _inputData.First().Length; j++)
+                {
+                
+                countzero = 0;
+                countone = 0;
+
+                for (int i = 0; i < _inputData.Count; i++)
+                    { 
+                    string s = _inputData[i];
+                    char c = s[j];
+                    if (c=='0')
+                        countzero++;
+                    else countone++;
+                    }
+
+                if (countone > countzero)
+                {
+                    gammarate += "1";
+                    epsilonrate += "0";
+                }
+                else
+                {
+                    gammarate += "0";
+                    epsilonrate += "1";
+                }
+                }
+            int decimalgamma = Convert.ToInt32(gammarate, 2);
+            int decimalepsilon = Convert.ToInt32(epsilonrate, 2);
+            powerconsumption = decimalepsilon * decimalgamma;
+
+
+
+            Console.WriteLine(gammarate + "/n" + decimalgamma);
+            Console.WriteLine(epsilonrate + "/n" + decimalepsilon);    
+            Console.WriteLine(powerconsumption);
+
+
+
+            Console.WriteLine("Hit any key to close this window...");
+            Console.ReadKey();
         }
+
+
+
         private static void readInput()
         {
             _inputData = System.IO.File.ReadAllLines("Input.txt").ToList();
         }
     }
 }
+
