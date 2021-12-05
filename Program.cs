a879f9b241ab3c317b3953b22e180b7d7b9e84af
Kai Jurtin <kai@jurtin.de>
2021-12-05 11:26:12 +0100

    Klasse für winLines erstellt

--- Program.cs
+++ Program.cs
@@ -25,26 +25,58 @@ namespace AdventOfCodeStartProject
 
             List<string> numbers = _inputData.First().Split(',').ToList();
             //List<string> blocksonly = _inputData[3].Split().Where(x => x != "").ToList();
-            List<string> blocksonly = new List<string>();
-            int arraylength = 5;
-            Array  [,] boxes = new Array[arraylength, arraylength];
+            List<string> bingoLine = new List<string>();
+            //int arraylength = 5;
+            //Array  [,] boxes = new Array[arraylength, arraylength];
+            int winningNumber = 0;
+            List<int> lineHits = new List<int>();
+            List<WinLine> winningLines= new List<WinLine>();
 
             for (int i = 2; i < _inputData.Count - 2; i++)
             {
 
-                blocksonly = _inputData[i].Split().Where(x => x != "").ToList();
+                bingoLine = _inputData[i].Split().Where(x => x != "").ToList();
+                //int counterLine = 0;
+                //int countercolumn = 0;
 
-                boxes[0, 0] = blocksonly[1].ToArray();
-                Console.WriteLine(blocksonly[1]);
+                int winningLine = 0;
+                int winningNumberIndex=0;
+                bool winnerLine = false;
 
-                for (int j=0; j < blocksonly.Count; j++)
+                for (int j=0; j < numbers.Count; j++)
                 {
-                    boxes[i, j] = blocksonly[j].ToArray();
-                    Console.WriteLine(boxes[i, j]);
+                    //bingoLine = bingoLine.Where(x=>x.Equals(numbers[j])).ToList();
+                    if (bingoLine.Contains(numbers[j]))
+                        lineHits.Add(Int32.Parse(numbers[j]));
+
+
+                    if (lineHits.Count == 5)
+                    {
+                        winnerLine = true;
+                        //winningNumber = Int32.Parse(numbers[j]);
+                        //winningNumberIndex = j;
+                        //winningLine = i;
+                        winningLines.Add(new WinLine(i, Int32.Parse(numbers[j]), j));
+                        break;
+                    }
+                    
                 }
+                lineHits.Clear();
+
+                if (winnerLine = true)
+                {
+                    //Console.WriteLine("Number: " + winningNumber);
+                    //Console.WriteLine("Number Index: " + winningNumberIndex);
+                    //Console.WriteLine("Line: " + winningLine);
+                foreach(object WinLine in winningLines)
+                    { Console.WriteLine(WinLine.ToString()); }
+                            
+                            //Console.ReadKey();
+                }
+                winnerLine = false;
+
 
             }
-
 
 
             //List <string> box = _inputData.Select(x => x.Split(" ").ToList();
@@ -69,3 +101,22 @@ namespace AdventOfCodeStartProject
             }
         }
     }
+class WinLine 
+{
+    public int LineIndex { get; set; }
+    public int Number { get; set; }
+    public int NumberIndex { get; set; }
+
+    public WinLine (int line, int number, int index)
+    {
+        LineIndex = line;
+        Number = number;
+        NumberIndex = index;
+    }
+    public override string ToString()
+    {
+        return LineIndex + " " + Number + " " + NumberIndex + " ";
+    }
+}
+
+
